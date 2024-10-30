a29a0ba259ccc6fb06aeb2db87c55b8a801bb5eb
Mhmd.K <38146273+maxswag@users.noreply.github.com>
2024-10-30 18:27:39 +0200

    Add files via upload

---
+++ js/js/js.js
@@ -0,0 +1,23 @@
+console.log(typeof undefined)// ?
+
+console.log(typeof 0) // ?
+
+console.log(typeof 10n)// ?
+
+console.log(typeof true)// ?
+
+console.log(typeof "foo") // ?
+
+console.log(typeof Symbol("id")) // ?
+
+console.log(typeof Math) // ?
+
+console.log(typeof null) // ?
+
+console.log(typeof alert) // ?
+
+console.log(typeof function () { }) // ?
+
+console.log(typeof {}) // ?
+
+console.log(typeof NaN) // ?
